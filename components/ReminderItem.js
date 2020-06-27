f8ff262b1595c6f4ba5c85be2924dfc3e3decd63
Chepman32 <53965045+Chepman32@users.noreply.github.com>
2020-06-27 19:29:40 +0300

    Update ReminderItem.js

--- components/ReminderItem.js
+++ components/ReminderItem.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { AppLoading } from 'expo';
-import { Container, Text, List, ListItem, Col, Grid, H1 } from 'native-base';
+import { Container, Text, List, ListItem, Col, Grid, H1, H3 } from 'native-base';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import { View, StyleSheet } from 'react-native';
 function ReminderItem( {route, navigation} ) {
-  const title = routes.params.title;
+  const title = route.params.title;
+  const date = route.params.date;
   console.log(title)
   return (
     <Container>
       <Grid>
           <Col style={{ backgroundColor: '#635DB7', height: 200, justifyContent: "center", alignItems: "center" }}>
-          <H1 style={{color: "white"}}>SSSSSSSSSSSS</H1>
+          <H1 style={{color: "white"}}>{ title } </H1>
+          </Col>
+          <Col style={{ backgroundColor: '#635DB7', height: 200, justifyContent: "center", alignItems: "center" }}>
+          <H3 style={{color: "white"}}>{ date } </H3>
           </Col>
         </Grid>
     </Container>
